4ce7dc702b6dcd2dd56b78bd160a95b77ceb741e
Marek Szumny <lukmarcus@gmail.com>
2024-10-13 23:39:47 +0200

    chore: cypress config files removed

--- cypress.config.components.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "cypress";
-import { config } from "./cypress.config";
-
-export default defineConfig({
-  projectId: "nnmunx",
-  component: config.component,
-});
--- cypress.config.e2e.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "cypress";
-import { config } from "./cypress.config";
-
-export default defineConfig({
-  projectId: "e5pnmx",
-  e2e: config.e2e,
-});

d41e67612354b03825a9a60cb108c78151d02b37
Marek Szumny <lukmarcus@gmail.com>
2025-01-11 02:48:54 +0100

    Certificates view bootstrap refactor

--- src/components/Certificate.vue
+++ src/components/Certificate.vue
@@ -1,36 +1,35 @@
 <template>
   <div
-    class="row flex-lg-row bg-white shadow-sm"
+    class="row flex-md-row bg-white shadow-sm"
     :class="[...certificate.filterTags]"
   >
-    <div class="col-auto certificate-media-holder ratio me-md-3">
-      <img
-        :src="'/src/images/certificates/' + certificate.image"
-        class="certificate-img"
-      />
-    </div>
-
-    <div class="col p-4">
-      <h4>{{ certificate.title }}</h4>
-      <ul class="certificate-meta list-inline mb-2">
-        <li class="list-inline-item me-3">
-          <font-awesome-icon :icon="['far', 'clock']" class="me-2" />{{
-            certificate.date
-          }}
-        </li>
-        <li class="list-inline-item me-3 certificate-type">
-          <font-awesome-icon class="me-2" icon="fa-regular fa-lightbulb" />
-          <span v-if="certificate.link">
-            <a :href="certificate.link">{{ certificate.source }}</a>
-          </span>
-          <span v-if="!certificate.link">{{ certificate.source }}</span>
-        </li>
-      </ul>
-      <div>
-        {{
-          certificate.description ||
-          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus penatibus et magnis dis parturient montes, nascetur ridiculus mus.."
-        }}
+    <div class="container my-4">
+      <div class="row align-items-center">
+        <div class="col-12 col-md-4 col-lg-5 mb-3 mb-md-0">
+          <img
+            :src="'/src/images/certificates/' + certificate.image"
+            class="img-fluid fixed-width"
+          />
+        </div>
+        <div class="col-12 col-md-8 col-lg-7">
+          <h4>{{ certificate.title }}</h4>
+          <ul class="certificate-meta list-inline mb-2">
+            <li class="list-inline-item me-3">
+              <font-awesome-icon :icon="['far', 'clock']" class="me-2" />
+              {{ certificate.date }}
+            </li>
+            <li class="list-inline-item">
+              <font-awesome-icon class="me-2" icon="fa-regular fa-lightbulb" />
+              <template v-if="certificate.link">
+                <a :href="certificate.link">{{ certificate.source }}</a>
+              </template>
+              <template v-else>{{ certificate.source }}</template>
+            </li>
+          </ul>
+          <div>
+            {{ certificate.description }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
